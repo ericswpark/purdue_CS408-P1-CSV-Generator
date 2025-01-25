00b2922e52fae63a2075b8be48b2dbed2dbb3c3f
Eric Park <me@ericswpark.com>
2025-01-25 15:17:31 -0500

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+const VALUE_RANGE: [i32; 7] = [0, 1, 2, 100, 199, 200, 201];
+
+fn main() {
+    println!("a,b,c,expected");
+
+    for a in VALUE_RANGE {
+        for b in VALUE_RANGE {
+            for c in VALUE_RANGE {
+                let result = triangle_type(a as usize, b as usize, c as usize);
+                println!("{},{},{},{}", a, b, c, result as i8);
+            }
+        }
+    }
+}
+
+enum TriangleType {
+    OutOfRange = -2,
+    Invalid = -1,
+    Scalene = 0,
+    Isosceles = 1,
+    Equilateral = 2,
+}
+
+fn triangle_type(a: usize, b: usize, c: usize) -> TriangleType {
+    if in_range(a) && in_range(b) && in_range(c) {
+        if a == b && b == c {
+            TriangleType::Equilateral
+        } else if a + b > c && a + c > b && b + c > a {
+            if a == b || b == c || a == c {
+                TriangleType::Isosceles
+            } else if a != b && b != c && a != c {
+                return TriangleType::Scalene;
+            } else {
+                panic!("Shouldn't happen");
+            }
+        } else {
+            TriangleType::Invalid
+        }
+    } else {
+        TriangleType::OutOfRange
+    }
+}
+
+fn in_range(n: usize) -> bool {
+    (1..=200).contains(&n)
+}
